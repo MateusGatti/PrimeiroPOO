132a329d8e9d0587fe5a72a822e6b71b2397566e
Mateus Mello <mt.gatti@hotmail.com>
2020-05-22 15:33:31 -0300

    atividade finalizada

---
+++ Carro.cs
@@ -0,0 +1,20 @@
+namespace AtividadeAula3
+{
+    public class Carro
+    {
+        public string nome;
+        public string cor;
+        public string Acelerar(){
+            return "O carro acelerou";
+        }
+        public string Freiar(){
+            return "O carro freiou";
+        }
+        public string Esquerda(){
+            return "O carro virou a esquerda";
+        }
+        public string Direita(){
+            return "O carro virou a direita";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AtividadeAula3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            carro.nome = "HB20";
+            carro.cor = "Preto";
+
+            Console.WriteLine(carro.nome);
+            Console.WriteLine(carro.cor);
+
+            Console.WriteLine(carro.Acelerar());
+            Console.WriteLine(carro.Freiar());
+            Console.WriteLine(carro.Esquerda());
+            Console.WriteLine(carro.Direita());
+
+
+
+        }
+    }
+}
